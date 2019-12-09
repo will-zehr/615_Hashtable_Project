861ffaf924ff7118cba8033bcfd714c45d5ed900
Liqinmg <57263209+Liqinmg@users.noreply.github.com>
2019-12-09 16:08:28 -0500

    Update Hash_matrix.py

--- Hash_matrix.py
+++ Hash_matrix.py
@@ -2,6 +2,7 @@ import time
 import copy
 import random
 import numpy as np
+import sys
 def hash_matrix(i,j,value,dims):
     i_set=list(set(i))
     j_set=list(set(j))
@@ -19,36 +20,45 @@ def hash_matrix(i,j,value,dims):
             h[ind][j[k]]=value[k]
     return h
 
+def play(h):
+    r=list(h.keys())
+    r.pop(r.index("dims"))
+    dim=h["dims"]
+    print(dim)
+    return type(h)
+
+def convert(h):
+    r=list(h.keys())
+    r.pop(r.index("dims"))
+    dim=h["dims"]
+    out=[[0]*int(dim[1]) for i in range(int(dim[0]))]
+    for i in r:
+        c=[int(k) for k in h[i].keys()]
+        for j in c:
+            out[int(i)-1][j-1]=h[i][str(j)]
+    return np.array(out)
+
 def plus(h1,h2):
-    print("#")
     h3=copy.deepcopy(h2)
-    posi1=h1["posi"]
-    posi2=h2["posi"]
-    for k in posi1:
-        if (k in h2):
-            h3[k]=h2[k]+h1[k]
-            spl=k.split(",")
-            h3["r"+spl[0]][int(spl[1])]=h3[k]
-            h3["c"+spl[1]][int(spl[0])]=h3[k]
+    r1=list(h1.keys())
+    r1.pop(r1.index("dims"))
+    for k in r1:
+        if k in h3:
+            c=h1[k].keys()
+            for j in c:
+                if j in h3[k]:
+                    h3[k][j]=h3[k][j]+h1[k][j]
+                else:
+                    h3[k][j]=h1[k][j]
         else:
-            h3[k]=h1[k]
-            h3["posi"].append(k)
-            spl=k.split(",")
-            h3["r"].append(int(spl[0]))
-            h3["r"]=list(set(h3["r"]))
-            h3["c"].append(int(spl[1]))
-            h3["c"]=list(set(h3["c"]))
-            h3["r"+spl[0]]=dict()
-            h3["c"+spl[1]]=dict()
-            h3["r"+spl[0]][int(spl[1])]=h1[k]
-            h3["c"+spl[1]][int(spl[0])]=h1[k]
+            h3[k]=copy.deepcopy(h1[k])
     return h3
 
 
 def multiply(h1,h2):
     h3=dict()
-    h3["dims"]=[h1["dims"][0],h2["dims"][1]]
-    r1=list(h1.keys())
+    h3["dims"]=[h1["dims"][0],h2["dims"][1]]  
+    r1=list(h1.keys())  
     r1.pop(r1.index("dims"))
     for i in r1:
         key=h1[i].keys()
@@ -64,16 +74,19 @@ def multiply(h1,h2):
                     else:
                         h3[i]=dict()
                         h3[i][k]=h1[i][j]*h2[j][k]
+                        
     return h3
     
 
 def main():
-    i=np.random.choice(range(1,10000000),100000,replace=True)
-    j=np.random.choice(range(1,10000000),100000,replace=True)
-    value=np.random.normal(0,1,100000)
+    '''i=np.random.choice(range(1,10000000),1000000,replace=True)
+    j=np.random.choice(range(1,10000000),1000000,replace=True)
+    value=np.random.normal(0,1,1000000)
     dims=[10000000,10000000]
     print("sample done")
-    t1=time.time()
+    h=hash_matrix(i,j,value,dims)
+    aa=multiply(h,h)'''
+    '''t1=time.time()
     h1=hash_matrix(i,j,value,dims)
     t2=time.time()
     h3=multiply(h1,h1)
@@ -85,10 +98,10 @@ def main():
     print(t2-t1)
     print(t3-t2)
     print(t4-t3)
-    print(t5-t4)
-    '''h=hash_matrix(i=[1,2,3,4],j=[5,6,7,8],value=[9,10,11,12],dims=[10,10])
-    h1=hash_matrix(i=[5,5,5,8],j=[1,2,3,4],value=[9,10,11,12],dims=[10,10])
-    print(multiply(h,h1))'''
+    print(t5-t4)'''
+    h=hash_matrix(i=[1,2,3,4],j=[1,2,3,4],value=[9,10,11,12],dims=[10,10])
+    h1=hash_matrix(i=[5,5,5,4],j=[1,2,3,8],value=[9,10,11,12],dims=[10,10])
+    print(convert(multiply(h,h)))
     return 0
 
 #main()
