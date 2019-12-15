e8d447425905820292108f21dfc0bef1898e629c
Liqinmg <57263209+Liqinmg@users.noreply.github.com>
2019-12-15 15:59:45 -0500

    Update Hash_matrix.py

--- Hash_matrix.py
+++ Hash_matrix.py
@@ -1,8 +1,5 @@
-import time
 import copy
-import random
 import numpy as np
-import sys
 def hash_matrix(i,j,value,dims):
     i_set=list(set(i))
     j_set=list(set(j))
@@ -19,24 +16,6 @@ def hash_matrix(i,j,value,dims):
         else:
             h[ind][j[k]]=value[k]
     return h
-
-def play(h):
-    r=list(h.keys())
-    r.pop(r.index("dims"))
-    dim=h["dims"]
-    print(dim)
-    return type(h)
-
-def convert(h):
-    r=list(h.keys())
-    r.pop(r.index("dims"))
-    dim=h["dims"]
-    out=[[0]*int(dim[1]) for i in range(int(dim[0]))]
-    for i in r:
-        c=[int(k) for k in h[i].keys()]
-        for j in c:
-            out[int(i)-1][j-1]=h[i][str(j)]
-    return np.array(out)
 
 def plus(h1,h2):
     h3=copy.deepcopy(h2)
@@ -76,32 +55,3 @@ def multiply(h1,h2):
                         h3[i][k]=h1[i][j]*h2[j][k]
                         
     return h3
-    
-
-def main():
-    '''i=np.random.choice(range(1,10000000),1000000,replace=True)
-    j=np.random.choice(range(1,10000000),1000000,replace=True)
-    value=np.random.normal(0,1,1000000)
-    dims=[10000000,10000000]
-    print("sample done")
-    h=hash_matrix(i,j,value,dims)
-    aa=multiply(h,h)'''
-    '''t1=time.time()
-    h1=hash_matrix(i,j,value,dims)
-    t2=time.time()
-    h3=multiply(h1,h1)
-    t3=time.time()
-    r=h1.keys()
-    t4=time.time()
-    d=h1["dims"]
-    t5=time.time()
-    print(t2-t1)
-    print(t3-t2)
-    print(t4-t3)
-    print(t5-t4)'''
-    h=hash_matrix(i=[1,2,3,4],j=[1,2,3,4],value=[9,10,11,12],dims=[10,10])
-    h1=hash_matrix(i=[5,5,5,4],j=[1,2,3,8],value=[9,10,11,12],dims=[10,10])
-    print(convert(multiply(h,h)))
-    return 0
-
-#main()
